687bd283e2ff828eed6f5f8b63a346edaef9a783
swagat030 <swagatkumarmishra888@gmail.com>
2024-01-02 15:59:51 +0530

    added responsive css

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import {RouterProvider, createBrowserRouter} from 'react-router-dom';
-import Login from './Login';
-import Browse from './Browse';
+import Login from "./Login";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Browse from "./Browse";
+
 
 const appRouter = createBrowserRouter([
     {
         path: "/",
-        element: <Login/>
+        element: <Login/>,
     },
     {
         path: "/browse",
-        element: <Browse/>
-    }
-])
+        element: <Browse/>,
+    },
+]);
+export const Body = () => {
 
-const Body = () => {
     return (
         <div>
             <RouterProvider router={appRouter}/>
         </div>
-    )
-}
+    );
+};
 
 export default Body;
-
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,9 @@
-import React, {useRef, useState} from 'react'
-import Header from './Header';
-import {LOGIN_LOGO_URL} from '../utils/constants';
-import {checkValidLoginData} from '../utils/validate';
+import React, {useRef, useState} from 'react';
+import Header from "./Header";
+import {LOGIN_LOGO_URL} from "../utils/constants";
+import {checkValidLoginData} from "../utils/validate";
 
-const Login = () => {
-
+export const Login = () => {
     const [isSignInForm, setIsSignInForm] = useState(true);
     const [errorMessage, setErrorMessage] = useState(null);
 
@@ -18,64 +17,60 @@ const Login = () => {
 
     const handleButtonClick = () => {
         let validationMessage;
-        console.log(email.current.value);
-        console.log(password.current.value);
         if (!fullName.current) {
             validationMessage = checkValidLoginData(email.current.value, password.current.value, null);
         } else {
-            console.log(fullName);
             validationMessage = checkValidLoginData(email.current.value, password.current.value, fullName.current.value);
         }
-        console.log(validationMessage);
         setErrorMessage(validationMessage);
-    }
+    };
 
     return (
-        <div>
+        <div className="relative h-screen">
             <Header/>
-            <div className="absolute">
+            <div className="absolute w-full h-full">
                 <img
                     src={LOGIN_LOGO_URL}
                     alt="logo"
+                    className="object-cover w-full h-full"
                 />
             </div>
             <form
                 onSubmit={(e) => e.preventDefault()}
-                className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+                className="w-full md:w-96 absolute p-6 md:p-12 bg-black my-8 md:my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+            >
                 <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-                {
-                    !isSignInForm && <input
-                        ref={fullName}
-                        type="text"
-                        placeholder="Full Name"
-                        className="p-4 my-4 w-full bg-gray-700"
-                    />
-                }
+                {!isSignInForm && <input
+                    ref={fullName}
+                    type="text"
+                    placeholder="Full Name"
+                    className="p-4 my-4 w-full bg-gray-700"
+                />}
                 <input
                     ref={email}
-                    className="p-4 my-4 w-full bg-gray-700"
                     type="text"
                     placeholder="Email Address"
+                    className="p-4 my-4 w-full bg-gray-700"
                 />
                 <input
                     ref={password}
-                    className="p-4 my-4 w-full bg-gray-700"
                     type="password"
                     placeholder="Password"
+                    className="p-4 my-4 w-full bg-gray-700"
                 />
-                <p className="text-red-500 font-bold text-lg">{errorMessage}</p>
+                <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
                 <button
-                    className="p-4 my-6 bg-red-700 w-full cursor-pointer"
+                    className="p-4 my-6 bg-red-700 w-full rounded-lg"
                     onClick={handleButtonClick}
                 >
                     {isSignInForm ? "Sign In" : "Sign Up"}
                 </button>
-                <p className="p-4 cursor-pointer"
-                   onClick={toggleSignInForm}
-                >{isSignInForm ? "New to Netflix? Sign up now" : "Already registered? Sign In now"}</p>
+                <p className="py-4 text-center cursor-pointer" onClick={toggleSignInForm}>
+                    {isSignInForm ? " New to Netflix? Sign Up Now" : "Already registered? Sign In Now"}
+                </p>
             </form>
         </div>
-    )
-}
+    );
+};
 
 export default Login;
